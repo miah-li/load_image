65e7a75f35e8d4b05ab46b53ce7c7d61f135a9c6
miah <1834091099@qq.com>
2020-12-15 18:07:18 +0800

    load_image1.sh
    Signed-off-by: miah <1834091099@qq.com>

--- load_image1.sh
+++ load_image1.sh
@@ -11,20 +11,20 @@ function load_image_ubuntu() {
 function get_id(){
 	artifacts=`curl \
 				-H "Accept: application/vnd.github.v3+json" \
-        		-u miah-li:cd36e32be5004cb216baa1c8b4accf890ce82a14 \
-        		https://api.github.com/repos/khadas/fenix/actions/artifacts `
+				-u miah-li:cd36e32be5004cb216baa1c8b4accf890ce82a14 \
+				https://api.github.com/repos/khadas/fenix/actions/artifacts `
 	a=`echo $artifacts | jq '.artifacts'`
 	echo $a | jq ' .[] | { id , name } ' > jason1.txt
-	a_id=`cat jason1.txt | awk -F"," '{ print $1 }' | sed -n '2p' `			# "id": 30587001
-	id=`echo $a_id | sed 's/ /\n/g' | sed -n '2p' `							# 30587001
+	a_id=`cat jason1.txt | awk -F"," '{ print $1 }' | sed -n '2p' `			
+	id=`echo $a_id | sed 's/ /\n/g' | sed -n '2p' `							
 	echo $id
 }
 
 function get_name(){
 	curl \
-		 -H "Accept: application/vnd.github.v3+json" \
-     	 -u miah-li:cd36e32be5004cb216baa1c8b4accf890ce82a14 \
-    	 https://api.github.com/repos/khadas/fenix/actions/artifacts/$id > jason2.txt
+		-H "Accept: application/vnd.github.v3+json" \
+		-u miah-li:cd36e32be5004cb216baa1c8b4accf890ce82a14 \
+		https://api.github.com/repos/khadas/fenix/actions/artifacts/$id > jason2.txt
 	name=` cat jason2.txt | jq .name | sed 's/\"//g' `
 	echo $name
 }
@@ -32,9 +32,9 @@ function get_name(){
 while [ -n "$id" ]
 do
 	curl \
-		 -H "Accept: application/vnd.github.v3+json" \
-     	 -u miah-li:cd36e32be5004cb216baa1c8b4accf890ce82a14 \
-     	 https://api.github.com/repos/khadas/fenix/actions/artifacts/$id > jason.txt
+		-H "Accept: application/vnd.github.v3+json" \
+		-u miah-li:cd36e32be5004cb216baa1c8b4accf890ce82a14 \
+		https://api.github.com/repos/khadas/fenix/actions/artifacts/$id > jason.txt
 	id=` cat jason.txt | jq .id `
 	name=` cat jason.txt | jq .name | sed 's/\"//g' `	 
 	echo $id
