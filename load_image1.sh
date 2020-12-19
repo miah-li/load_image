c9aec1083e69233f8ded196cca1770b27049db84
miah <1834091099@qq.com>
2020-12-19 15:46:26 +0800

    load_images
    Signed-off-by: miah <1834091099@qq.com>

--- load_image1.sh
+++ load_image1.sh
@@ -1,9 +1,11 @@
-id=$1
+uname=$1
+token=$2
+id=$3
 
 function load_image_ubuntu() {      
 	curl -L -v \
 		-H "Accept: application/vnd.github.v3+json" \
-		-u miah-li:54bfe8c0f08c3f25237e8e132299c19e9f432b6b  \
+		-u $uname:$token  \
 		-o $name.zip \
  		https://api.github.com/repos/khadas/fenix/actions/artifacts/$id/zip
  }
@@ -11,7 +13,7 @@ function load_image_ubuntu() {
 function get_id(){
 	artifacts=`curl \
 		-H "Accept: application/vnd.github.v3+json" \
-		-u miah-li:54bfe8c0f08c3f25237e8e132299c19e9f432b6b  \
+		-u $uname:$token  \
 		https://api.github.com/repos/khadas/fenix/actions/artifacts `
 	a=`echo $artifacts | jq '.artifacts'`
 	echo $a | jq ' .[] | { id , name } ' > jason1.txt
@@ -23,39 +25,43 @@ function get_id(){
 function get_name(){
 	curl \
 		-H "Accept: application/vnd.github.v3+json" \
-		-u miah-li:54bfe8c0f08c3f25237e8e132299c19e9f432b6b  \
+		-u $uname:$token  \
 		https://api.github.com/repos/khadas/fenix/actions/artifacts/$id > jason2.txt
 	name=` cat jason2.txt | jq .name | sed 's/\"//g' `
 	echo $name
 }
 
-while [ -n "$id" ]
-do
-	curl \
-		-H "Accept: application/vnd.github.v3+json" \
-		-u miah-li:54bfe8c0f08c3f25237e8e132299c19e9f432b6b  \
-		https://api.github.com/repos/khadas/fenix/actions/artifacts/$id > jason.txt
-	id=` cat jason.txt | jq .id `
-	name=` cat jason.txt | jq .name | sed 's/\"//g' `	 
-	echo $id
-	echo $name
-	load_image_ubuntu
-done
-
-while [ ! -n "$id" ]
-do
-	get_id
-	for(( i=1; i<=50 ; i++ ))
+if [ -n "$uname" ] && [ -n "$token" ]; then
+	while [ -n "$id" ]
 	do
-		get_name
-		if [ "$name" != "null" ]; then
-			load_image_ubuntu
-			id=$((id-1))
-			echo $id
-		else
-			break
-		fi
+		curl \
+			-H "Accept: application/vnd.github.v3+json" \
+			-u $uname:$token  \
+			https://api.github.com/repos/khadas/fenix/actions/artifacts/$id > jason.txt
+		id=` cat jason.txt | jq .id `
+		name=` cat jason.txt | jq .name | sed 's/\"//g' `	 
+		echo $id
+		echo $name
+		load_image_ubuntu
 	done
-	break
-done
+
+	while [ ! -n "$id" ]
+	do
+		get_id
+		for(( i=1; i<=50 ; i++ ))
+		do
+			get_name
+			if [ "$name" != "null" ]; then
+				load_image_ubuntu
+				id=$((id-1))
+				echo $id
+			else
+				break
+			fi
+		done
+		break
+	done
+else
+	echo "I don't understand!"
+fi
 
